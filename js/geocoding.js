81f24e1084adc479b00ff77a5c4a3969e43e82bb
Harry Ghazni <hghazni@gmail.com>
2019-07-31 23:44:03 +0100

    error  handling

--- js/geocoding.js
+++ js/geocoding.js
@@ -2,7 +2,7 @@ var x = document.getElementById("geocoding");
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition, ShowError);
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
@@ -11,4 +11,21 @@ function getLocation() {
 function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
 }
